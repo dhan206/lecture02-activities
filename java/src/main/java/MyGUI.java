11700a4bf73eba91d4e2f7715447e5550bcbc28f
DereK Han <derekhan206@gmail.com>
2017-03-30 14:11:18 -0700

    Adds MyGui code

--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -6,24 +6,47 @@ import java.util.*;
  * A demo of GUI programming with Swing
  */
 public class MyGUI extends JFrame {
-	
+
 	public MyGUI() {
-		super("The Skywalker Family");
+		super("My GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton theButton = new JButton("Click me!");
+		JButton theButton = new JButton("Button #1!");
 
+		theButton.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("1");
+			}
+		});
 
+		JButton theButton2 = new JButton("Button #2!");
 
+		theButton2.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("2");
+			}
+		});
 
+		JPanel container = new JPanel();
+
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS ));
+		container.add( theButton );
+		container.add( theButton2 );
+
+		JPanel otherContainer = new JPanel();
+		otherContainer.setLayout(new BoxLayout(otherContainer, BoxLayout.X_AXIS));
+		otherContainer.add(new JButton("A"));
+		otherContainer.add(new JButton("B"));
+		otherContainer.add(new JButton("C"));
+
+		container.add(otherContainer);
 		
-		this.add(theButton);
-		
-		this.pack();
-		this.setVisible(true);		
+		this.add( container );
+		this.pack(); // resize so all content is visible and contained
+		this.setVisible( true );		
 	}
-	
+
 	public static void main(String[] args) {
-    new MyGUI(); //create the frame
-  }
+		new MyGUI(); //create the frame
+	}
 }
